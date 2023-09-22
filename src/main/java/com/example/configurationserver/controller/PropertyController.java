a57a70d2047d68fcb0039f8fb89868d1a8c52404
HP <mkalikin@mail.ru>
2023-09-22 16:20:40 +0400

    added config cloud server

---
+++ src/main/java/com/example/configurationserver/controller/PropertyController.java
@@ -0,0 +1,20 @@
+package com.example.configurationserver.controller;
+
+import com.example.configurationserver.configuration.Config;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/properties")
+public class PropertyController {
+
+    private final Config config;
+    @GetMapping
+    public String getTestProperty() {
+        return config.getTestProperty();
+    }
+}
